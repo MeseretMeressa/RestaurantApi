10d35734349b4c4a8e0ca7438336cf9cca28ad11
MeseretMeressa <meseret.bisrat@gmail.com>
2016-03-17 14:04:05 -0400

    initial files

---
+++ MvcApplication1/Controllers/OrderController.cs
@@ -0,0 +1,51 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class OrderController : ApiController
+    {
+        // GET api/order
+        public IEnumerable<Order> Get()
+        {
+            Order sample = new Order();
+            OrderItem item = new OrderItem();
+            item.item = "Girgiro";
+            item.itemQuantity =2;
+            item.message = "Not so hot";
+
+            sample.tableNumber = 1345;
+            sample.orderId = "12345";
+            sample.orderItems = new OrderItem[] { item };
+            //You fetch data from the DB
+            //display the result
+            return new Order[] { sample,new Order(), new Order() ,new Order()};
+        }
+
+        // GET api/order/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/order
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/order/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/order/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MvcApplication1/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Order
+    {
+        public string orderId{get;set;}
+        public int tableNumber{get;set;}
+        public OrderItem[] orderItems { get; set; }
+
+    }
+}
